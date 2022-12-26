3e6a9b22817c58d8a6a9304dd4cfb901c6b310b6
Mustafa Bebek <mmustafab324@gmail.com>
2022-12-27 00:32:09 +0300

    configration

--- WebProgramingProject/Data/MovieDbContext.cs
+++ WebProgramingProject/Data/MovieDbContext.cs
@@ -1,9 +1,5 @@
 ﻿
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Text;
 using WebProgramingProject.Models;
 
 namespace WebProgramingProject.Data
